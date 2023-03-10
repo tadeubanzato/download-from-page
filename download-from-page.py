c6c6bdd5b329fc7e8e6d9d03f6afbb38e7edf4a6
Tadeu Banzato <10622335+tadeubanzato@users.noreply.github.com>
2023-03-09 22:54:18 -0800

    Updates

--- download-from-page.py
+++ download-from-page.py
@@ -2,36 +2,49 @@
 # -*- coding: utf-8 -*-
 # download-from-page.py
 
-import re
+import os
 import requests
+import html5lib
 from bs4 import BeautifulSoup
 
-url = 'https://www.uol.com.br'
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html5lib')
+def check_folder(folder):
+    folder = ('downloads')
+    check_folder = os.path.isdir(folder)
+    return check_folder 
 
-img_tags = soup.find_all('img')
-urls = [img['src'] for img in img_tags]
+def main(objects,folder):
+    for object in objects:
+        
+        print(object)
+        ext_check = ['.svg','.jpg','.png','.gif']
+        
+        for ext in ext_check:
+            if ext in object:
+                file_url = object.split(ext)[0] + ext
+                file_name = object.split(ext)[0].split('/')[-1] + ext
+                print(f'Downloading: {file_name}')
+                
+                try:
+                    r = requests.get(file_url, allow_redirects=True)
+                    open(f'{folder}/{file_name}', 'wb').write(r.content)
+                except:
+                    print('skip')
+                
+if __name__ == '__main__':
+    folder = ('downloads')
+    checked = check_folder(folder)
+    
+    if checked:
+        print(folder, "folder already exists.")
+    else:
+        os.makedirs(folder)
+        print("created folder : ", folder)
+    
+    url = 'https://www.uol.com.br'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html5lib')
 
-for url in urls:
+    img_tags = soup.find_all('img')
+    objects = [img['src'] for img in img_tags]
     
-    
-    
-    
-    filename = re.search(r'/(jpg|gif|png|svg|webp)$', url)
-    print(filename)
-    if not filename:
-         print("Regex didn't match with the url: {}".format(url))
-         continue
-    with open(filename.group(1), 'wb') as f:
-        if 'http' not in url:
-            # sometimes an image source can be relative 
-            # if it is provide the base url which also happens 
-            # to be the site variable atm. 
-            url = '{}{}'.format(site, url)
-            print(url)
-        response = requests.get(url)
-        f.write(response.content)
-
-
-open('facebook.ico', 'wb').write(r.content)
+    main(objects,folder)
